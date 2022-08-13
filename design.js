a8071114b40e425790e4a8d3c0a028d5bce9e06b
Sahil K <45844195+sahilsingh511@users.noreply.github.com>
2022-08-14 01:12:17 +0530

    Update design.js

--- design.js
+++ design.js
@@ -10,3 +10,11 @@ for (let i = 0; i < spanValue.length; i++) {
     spanValue[i].textContent = e.target.value;
   });
 }
+
+var typed = new Typed(".personality", {
+  strings: ["Web Developer", "Web Designer", "Youtuber"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  smartBackspace: true,
+  loop: true
+});
